eeced267930571b50798a7ca026183fa8e916560
Dariy Olesya <oldariy95@gmail.com>
2020-04-24 19:20:00 +0600

    adaptiv header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -23,18 +23,24 @@ $(function(){
     });
 
     $('.icon-th-list').on('click', function(){
-        $('.product__item').addClass('list');
+        $('.product-page__items .product__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active')
     });
 
     $('.icon-th-large').on('click', function(){
-        $('.product__item').removeClass('list');
+        $('.product-page__items .product__item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active')
     });
 
+    $('.menu__btn').on('click',function(){
+        $('.menu__list').slideToggle();
+    });
 
+    $('.header__btn-menu').on('click',function(){
+        $('.header__box').toggleClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
 });
