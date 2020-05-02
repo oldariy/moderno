f42ffed24d983583abb0158d9e607fd8aec0fe36
Dariy Olesya <oldariy95@gmail.com>
2020-05-02 08:08:28 +0600

    added upload page

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,10 @@ $(function () {
     $('.icon-th-list').removeClass('active')
   });
 
+  $('input[type="file"], select').styler();
+
+
+
   $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
   });
